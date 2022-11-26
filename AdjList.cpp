786d94cdde5c1636abf9b00a3e65c091a70e1ed3
Anirudh Bachiraju <alb11@illinois.edu>
2022-11-26 22:39:44 +0000

    Added Adjacency List

---
+++ AdjList.cpp
@@ -0,0 +1,23 @@
+#include "AdjList.h"
+
+AdjList::AdjList(const std::vector<std::vector<int>> & dataset, int maxNode) {
+    std::vector<std::vector<int>> adjlist;
+    adjlist.resize(maxNode);
+    for (std::vector<int> nodePair : dataset) {
+        int fromNode = nodePair[0];
+        int toNode = nodePair[1];
+        adjlist[toNode].push_back(fromNode);
+    }
+    adjlist_ = adjlist;
+    maxNode_ = maxNode;
+}
+
+void AdjList::print() {
+    for (int i = 0; i < maxNode_; i++) {
+        std::cout << i << " <- ";
+        for (int node : adjlist_[i]) {
+            std::cout << node << " ";
+        }
+        std::cout << "" << std::endl;
+    }
+}
